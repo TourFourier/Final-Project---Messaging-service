83d0df9d14773ee993e7ae4a5c628d5711aa109c
TourFourier <43444695+TourFourier@users.noreply.github.com>
2019-04-08 16:02:41 +0300

    Add project files.

---
+++ common/CSafeMessageQueue.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class  CSafeMessageQueue
+{
+	CRITICAL_SECTION m_cs;
+	std::queue<IMessage&> m_qMessageQueue;
+public:
+	CSafeMessageQueue();
+	~CSafeMessageQueue();
+
+	void Push(IMessage& message);
+	IMessage& Pop();
+};
+
+
+
